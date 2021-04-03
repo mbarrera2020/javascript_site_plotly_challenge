20a3606b1d14f225aefe6fa78a264ebc2969b873
Maria Barrera <mbarrera_usa@yahoo.com>
2021-04-03 16:20:41 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -44,7 +44,52 @@ function initialize(){
 //      the top 1O OTUs found in that individual.
 // ---------------------------------------------------------------------
 
-// <insert code to display barchart>
+function fn_barChart(subjectID){
+  d3.json('samples.json').then((data)=>{
+      var samples = data.samples;
+
+      // Test / display data
+      console.log(samples)
+
+      var ID = samples.map(row=>row.id).indexOf(subjectID);
+
+      console.log(ID)
+
+      // Use otu_ids as the labels for the bar chart.
+      var otuID = samples.map(row=>row.otu_ids);
+      var otuIDTopTen = otuID[ID].slice(0,10);
+
+      console.log(otuIDTopTen)
+
+      // Use sample_values as the values for the bar chart.
+      var sampleValue = samples.map(row=>row.sample_values);
+      var sampleValueTopTen = sampleValue[ID].slice(0,10);
+      
+      // Use otu_labels as the hovertext for the chart.
+      var otuLabelTopTen = samples.map(row=>row.otu_labels).slice(0,10);
+      
+      var trace={
+          x: sampleValueTopTen,
+          y: otuIDTopTen.map(otu =>`OTU ${otu}  `),
+          text: otuLabelTopTen,
+          type:'bar'
+      }
+      // ------------------------------------------
+      // Setup chart layout with title & margins
+      // ------------------------------------------
+      var barLayout = {
+        title: "Top 10 OTUs",
+        xaxis: {title: "Sample Values"},
+        // yaxis: { title: "OTU IDs"},
+        margin: {t: 80, l: 175}
+      };
+
+      // ------------------------------------------
+      // Display the bar chart            
+      // ------------------------------------------
+      Plotly.newPlot('bar', [trace], barLayout);
+  })
+};
 
 // ------------------------------------------------------------------------------------
 // 3. Create a bubble chart that displays each sample.   
