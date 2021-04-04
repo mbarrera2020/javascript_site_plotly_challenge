d94b21986724a1f41ce63614216949cb9cffa39d
Maria Barrera <mbarrera_usa@yahoo.com>
2021-04-03 17:08:03 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -43,52 +43,52 @@ function initialize(){
 // 2. Create a horizontal bar chart for selected Subject ID to display 
 //      the top 1O OTUs found in that individual.
 // ---------------------------------------------------------------------
-  function fn_barChart(subjectID){
-      d3.json('samples.json').then((data)=>{
-          var samples = data.samples;
+function fn_barChart(subjectID){
+  d3.json('samples.json').then((data)=>{
+      var samples = data.samples;
 
-          // Test / display data
-          console.log(samples)
-  
-          var ID = samples.map(row=>row.id).indexOf(subjectID);
+      // Test / display data
+      console.log(samples)
 
-          console.log(ID)
+      var ID = samples.map(row=>row.id).indexOf(subjectID);
 
-          // Use otu_ids as the labels for the bar chart.
-          var otuID = samples.map(row=>row.otu_ids);
-          var otuIDTopTen = otuID[ID].slice(0,10);
+      console.log(ID)
 
-          console.log(otuIDTopTen)
+      // Use otu_ids as the labels for the bar chart.
+      var otuID = samples.map(row=>row.otu_ids);
+      var otuIDTopTen = otuID[ID].slice(0,10).reverse();
 
-          // Use sample_values as the values for the bar chart.
-          var sampleValue = samples.map(row=>row.sample_values);
-          var sampleValueTopTen = sampleValue[ID].slice(0,10);
-          
-          // Use otu_labels as the hovertext for the chart.
-          var otuLabelTopTen = samples.map(row=>row.otu_labels).slice(0,10);
-          
-          var trace={
-              x: sampleValueTopTen,
-              y: otuIDTopTen.map(otu =>`OTU ${otu}  `),
-              text: otuLabelTopTen,
-              type:'bar'
-          }
-          // ------------------------------------------
-          // Setup chart layout with title & margins
-          // ------------------------------------------
-          var barLayout = {
-            title: "Top 10 OTUs",
-            xaxis: {title: "Sample Values"},
-            // yaxis: { title: "OTU IDs"},
-            margin: {t: 80, l: 175}
-          };
+      console.log(otuIDTopTen)
 
-          // ------------------------------------------
-          // Display the bar chart            
-          // ------------------------------------------
-          Plotly.newPlot('bar', [trace], barLayout);
-      })
-  };
+      // Use sample_values as the values for the bar chart.
+      var sampleValue = samples.map(row=>row.sample_values);
+      var sampleValueTopTen = sampleValue[ID].slice(0,10).reverse();
+      
+      // Use otu_labels as the hovertext for the chart.
+      var otuLabelTopTen = samples.map(row=>row.otu_labels).slice(0,10);
+      
+      var trace={
+          x: sampleValueTopTen,
+          y: otuIDTopTen.map(otu =>`OTU ${otu}  `),
+          text: otuLabelTopTen,
+          type:'bar'
+      }
+      // ------------------------------------------
+      // Setup chart layout with title & margins
+      // ------------------------------------------
+      var barLayout = {
+        title: "Top 10 OTUs",
+        xaxis: {title: "Sample Values"},
+        // yaxis: { title: "OTU IDs"},
+        margin: {t: 80, l: 175}
+      };
+
+      // ------------------------------------------
+      // Display the bar chart            
+      // ------------------------------------------
+      Plotly.newPlot('bar', [trace], barLayout);
+  })
+};
 
 // ------------------------------------------------------------------------------------
 // 3. Create a bubble chart that displays each sample.   
